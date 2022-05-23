d67ee8d8d4401a8cfa15e5badac6a336222c4636
sngeza <geza.simon@dpc.hu>
2022-05-23 09:59:50 +0200

    endstate 1.0

--- tutorial/app/src/main/java/com/example/tutorial/ForceAuthInterceptor.java
+++ tutorial/app/src/main/java/com/example/tutorial/ForceAuthInterceptor.java
@@ -1,3 +1,10 @@
+/*
+ *  Copyright (c) 2022 ForgeRock. All rights reserved.
+ *
+ *  This software may be modified and distributed under the terms
+ *  of the MIT license. See the LICENSE file for details.
+ */
+
 package com.example.tutorial;
 
 import static org.forgerock.android.auth.Action.START_AUTHENTICATE;
--- tutorial/app/src/main/java/com/example/tutorial/SelectIdpDialogFragment.java
+++ tutorial/app/src/main/java/com/example/tutorial/SelectIdpDialogFragment.java
@@ -1,10 +1,11 @@
-package com.example.tutorial;
 /*
- * Copyright (c) 2021 ForgeRock. All rights reserved.
+ *  Copyright (c) 2022 ForgeRock. All rights reserved.
  *
- * This software may be modified and distributed under the terms
- * of the MIT license. See the LICENSE file for details.
+ *  This software may be modified and distributed under the terms
+ *  of the MIT license. See the LICENSE file for details.
  */
+
+package com.example.tutorial;
 
 
 import android.content.Context;
